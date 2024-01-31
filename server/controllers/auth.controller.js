414630b59f85dbdf3c041c874189d316d8f05a02
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2024-01-31 16:47:13 +0300

    login API

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/User");
 const { sendingSignupCode } = require("../services/auth.service");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 
 async function signup(req, res) {
   const { email, password, firstName, lastName } = req.body;
@@ -24,7 +26,39 @@ function sendCode(req, res) {
   res.json({ code: newCode });
 }
 
+async function login(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Email ou Mot de Passe Invalide' });
+    }
+
+    // const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = password === user.password;
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Email ou Mot de Passe Invalide' });
+    }
+
+    // Generate a JWT token for authentication
+    const token = jwt.sign(
+        { userId: user._id, email: user.email, role: user.role },
+        process.env.PRIVATE_KEY,
+        { expiresIn: '5h' }
+    );
+
+    res.json({ token, userId: user._id, role: user.role });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}
+
 module.exports = {
   signup,
   sendCode,
+  login
 };
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 router.post("/send-code", authController.sendCode);
 
 module.exports = router;
