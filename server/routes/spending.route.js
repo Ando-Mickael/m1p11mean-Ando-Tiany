8a65b12f3c9064ee8721dfb2c03fdea5660a37a3
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2024-02-04 17:19:51 +0300

    API get monthly & daily spendings

--- server/routes/spending.route.js
+++ server/routes/spending.route.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const spendingController = require("../controllers/spending.controller");
+const {sendResponse} = require("../utils/middlewares");
 
 router.get("/", spendingController.getSpendings);
 router.post("/", spendingController.create);
+
+router.get("/:year", spendingController.getMonthlySpendings, sendResponse("result"));
+router.get("/:year/:month", spendingController.getDailySpendings, sendResponse("result"));
+
 
 module.exports = router;
