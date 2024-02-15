66574f42270fae060d0ead9b86b8e9562de35817
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2024-02-15 21:59:53 +0300

    statistiques front

--- client/src/app/manager/dashboard/manager-statistiques.component.ts
+++ client/src/app/manager/dashboard/manager-statistiques.component.ts
@@ -3,19 +3,63 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'manager-statistiques',
   template: `
-    <div>
+    <div class="container">
       <p>Statistiques</p>
-      <h1>Bénéfices</h1>
-      <manager-daily-revenue></manager-daily-revenue>
-      <manager-monthly-revenue></manager-monthly-revenue>
-      <h1>Chiffres d'affaire</h1>
-      <manager-daily-ca></manager-daily-ca>
-      <manager-monthly-ca></manager-monthly-ca>
-      <div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <h1>Bénéfices</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <manager-daily-revenue></manager-daily-revenue>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <manager-monthly-revenue></manager-monthly-revenue>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-3">
+        <div class="col-md-12">
+          <h1>Chiffres d'affaire</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <manager-daily-ca></manager-daily-ca>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-body">
+              <manager-monthly-ca></manager-monthly-ca>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="my-4">
         <h2>Total monthly appointments</h2>
-        <manager-total-monthly-appointments></manager-total-monthly-appointments>
+        <div class="card">
+          <div class="card-body">
+            <manager-total-monthly-appointments></manager-total-monthly-appointments>
+          </div>
+        </div>
       </div>
     </div>
+
   `,
   styles: [],
 })
