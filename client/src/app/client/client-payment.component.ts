e70c8496b9b4ee240365141bf14035ec15d5262d
Ando-Mickael <ando.mickael25@gmail.com>
2024-02-20 16:41:54 +0300

    paiement front

--- client/src/app/client/client-payment.component.ts
+++ client/src/app/client/client-payment.component.ts
@@ -5,41 +5,82 @@ import { ConfigService } from '../config.service';
 @Component({
   selector: 'client-home',
   template: `
-    <form (ngSubmit)="onSubmit($event)">
-      <h2>Payment</h2>
-      <div>
-        <label for="">Card number</label>
-        <input
-          type="text"
-          name="cardNumber"
-          [(ngModel)]="cardNumber"
-          placeholder="XXXX XXXX XXXX XXXX"
-        />
+    <section class="ftco-section">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-xl-10 ftco-animate">
+            <form (ngSubmit)="onSubmit($event)" class="billing-form">
+              <h3 class="mb-4 billing-heading">Détails de paiement</h3>
+              <div class="row align-items-end">
+                <div class="col-md-12">
+                  <div class="form-group">
+                    <label for="cardNumber">Numéro de carte</label>
+                    <input
+                      id="cardNumber"
+                      type="text"
+                      class="form-control"
+                      name="cardNumber"
+                      [(ngModel)]="cardNumber"
+                      placeholder="XXXX XXXX XXXX XXXX"
+                    />
+                  </div>
+                </div>
+                <div class="w-100"></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="country">Pays</label>
+                    <div class="select-wrap">
+                      <div class="icon">
+                        <span class="ion-ios-arrow-down"></span>
+                      </div>
+                      <select name="" id="country" class="form-control">
+                        <option value="">Madagascar</option>
+                        <option value="">France</option>
+                        <option value="">Etas Unis</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="postcodezip">Code postal</label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      placeholder="90210"
+                    />
+                  </div>
+                </div>
+                <div class="w-100"></div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="expiration">Exipiration</label>
+                    <input id="expiration" type="date" class="form-control" />
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label for="cvv">CVV</label>
+                    <input
+                      id="cvv"
+                      type="text"
+                      class="form-control"
+                      placeholder="CVV"
+                    />
+                  </div>
+                </div>
+                <div class="w-100"></div>
+              </div>
+              <div>
+                <button class="btn btn-primary w-100 px-4 py-3" type="submit">
+                  Payer
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <label for="">CVV</label>
-        <input type="text" placeholder="CVV" />
-      </div>
-      <div>
-        <label for="">Expiry</label>
-        <input type="date" />
-      </div>
-      <div>
-        <label for="">Code postal</label>
-        <input type="text" placeholder="90210" />
-      </div>
-
-      <div>
-        <label for="">Country</label>
-        <select name="" id="">
-          <option value="">Madagascar</option>
-          <option value="">France</option>
-          <option value="">United States</option>
-        </select>
-      </div>
-
-      <button type="submit">Pay</button>
-    </form>
+    </section>
   `,
 })
 export class ClientPaymentComponent {
