9b8a0503ec443464be005b42ec085b497c5472e6
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2024-02-28 18:12:41 +0300

    fixed offre speciale

--- client/src/app/manager/special-offers/manager-special-offer.component.ts
+++ client/src/app/manager/special-offers/manager-special-offer.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { SpecialOfferService } from '../../services/special-offer.service';
-import {Service} from "../../core/service.component";
-import {ServiceService} from "../../services/service.service";
+import { Service } from "../../core/service.component";
+import { ServiceService } from "../../services/service.service";
+
 @Component({
   selector: 'manager-special-offer',
   template: `
@@ -13,42 +14,46 @@ import {ServiceService} from "../../services/service.service";
             <div>
               <h2>Créer une Offre Spéciale</h2>
 
-              <form [formGroup]="specialOfferForm" (ngSubmit)="onSubmit()" class="billing-form">
-                <div class="form-group">
-                  <label for="name">Nom:</label>
-                  <input type="text" id="name" formControlName="name" class="form-control" placeholder="">
+              <form [formGroup]="specialOfferForm" (ngSubmit)="onSubmit()">
+                <label for="name">Name</label>
+                <input type="text" id="name" formControlName="name">
+                <div *ngIf="specialOfferForm.get('name').invalid && specialOfferForm.get('name').touched">
+                  <small>Name is required</small>
                 </div>
-                <div class="form-group">
-                  <label for="description">Description:</label>
-                  <textarea id="description" formControlName="description" class="form-control" placeholder=""></textarea>
+
+                <label for="description">Description</label>
+                <textarea id="description" formControlName="description"></textarea>
+                <div *ngIf="specialOfferForm.get('description').invalid && specialOfferForm.get('description').touched">
+                  <small>Description is required</small>
                 </div>
-                <div class="form-group">
-                  <label for="startDate">Date de début:</label>
-                  <input type="date" id="startDate" formControlName="startDate" class="form-control" placeholder="">
+
+                <label for="startDate">Start Date</label>
+                <input type="date" id="startDate" formControlName="startDate">
+                <div *ngIf="specialOfferForm.get('startDate').invalid && specialOfferForm.get('startDate').touched">
+                  <small>Start Date is required</small>
                 </div>
-                <div class="form-group">
-                  <label for="endDate">Date de fin:</label>
-                  <input type="date" id="endDate" formControlName="endDate" class="form-control" placeholder="">
+
+                <label for="endDate">End Date</label>
+                <input type="date" id="endDate" formControlName="endDate">
+                <div *ngIf="specialOfferForm.get('endDate').invalid && specialOfferForm.get('endDate').touched">
+                  <small>End Date is required</small>
                 </div>
-                <div class="form-group">
-                  <table class="table">
-                    <thead>
-                    <tr>
-                      <th>Service</th>
-                      <th>Percentage (%)</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr *ngFor="let service of services">
-                      <td>{{ service.name }}</td>
-                      <td><input type="number" [formControlName]="service?._id!" class="form-control"></td>
-                    </tr>
-                    </tbody>
-                  </table>
+
+                <div formArrayName="percentages">
+                  <div *ngFor="let percentageGroup of specialOfferForm.get('percentages')?.controls; let i = index">
+                    <div [formGroupName]="i">
+                      <label>{{ services[i].name }}</label>
+                      <input type="number" formControlName="percentage">
+                      <div *ngIf="percentageGroup.get('percentage')?.invalid && percentageGroup.get('percentage')?.touched">
+                        <small>Percentage is required</small>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div class="w-100"></div>
-                <button type="submit" class="btn btn-primary py-3 px-4">Create Special Offer</button>
+
+                <button type="submit" [disabled]="specialOfferForm.invalid">Submit</button>
               </form>
+
             </div>
           </div>
         </div>
@@ -56,22 +61,24 @@ import {ServiceService} from "../../services/service.service";
     </section>
   `,
 })
-export class ManagerSpecialOfferComponent {
-  specialOfferForm: FormGroup;
+export class ManagerSpecialOfferComponent implements OnInit {
+  specialOfferForm!: FormGroup;
   services: Service[] = [];
 
   constructor(
-    private formBuilder: FormBuilder,
     private serviceService: ServiceService,
     private specialOfferService: SpecialOfferService
-  ) {
-    this.specialOfferForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      description: ['', Validators.required],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required],
-      percentages: this.formBuilder.array([]) // Initialize an empty form array
+  ) {}
+
+  ngOnInit() {
+    this.specialOfferForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      startDate: new FormControl('', Validators.required),
+      endDate: new FormControl('', Validators.required),
+      percentages: new FormArray([]),
     });
+
     this.loadServices();
   }
 
@@ -88,37 +95,35 @@ export class ManagerSpecialOfferComponent {
   }
 
   initializeFormControls() {
-    const percentagesFormArray = this.specialOfferForm.get('percentages') as FormArray; // Cast to FormArray
-    if (percentagesFormArray) {
-      this.services.forEach((service) => {
-        if (service._id) { // Check if _id is defined
-          percentagesFormArray.push(this.formBuilder.group({
-            serviceId: service._id,
-            percentage: [0, Validators.required] // Set the default value to 0
-          }));
-        }
-      });
-    }
+    const percentagesFormArray = this.specialOfferForm.get('percentages') as FormArray;
+    this.services.forEach((service) => {
+      if (service && service._id) {
+        percentagesFormArray.push(new FormGroup({
+          serviceId: new FormControl(service._id),
+          percentage: new FormControl('0', Validators.required),
+        }));
+      }
+    });
   }
 
   onSubmit() {
-    if (this.specialOfferForm.valid) {
+    if (this.specialOfferForm && this.specialOfferForm.valid) {
       const { name, description, startDate, endDate, percentages } = this.specialOfferForm.value;
 
       this.specialOfferService.createSpecialOffer({
         name,
         description,
-        percentages, // Send the entire percentages array
+        percentages,
         startDate,
         endDate
       }).subscribe(
         (createdOffer) => {
           console.log('Special offer created successfully', createdOffer);
-          // You can handle success, e.g., show a success message
+          // Handle success, e.g., show a success message
         },
         (error) => {
           console.error('Error creating special offer', error);
-          // You can handle errors, e.g., show an error message
+          // Handle errors, e.g., show an error message
         }
       );
     }
