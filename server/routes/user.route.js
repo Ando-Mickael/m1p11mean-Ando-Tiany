0962e17671887e2bc74418460dc24c323b89a424
Ando Mickael Ramahenina <ando.mickael25@gmail.com>
2024-02-28 15:37:14 +0300

    fix update employee in manager

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const { sendResponse } = require("../utils/middlewares");
 
+router.get("/", userController.getUsers);
 router.get("/:id", userController.getUserById, sendResponse("user"));
 router.put("/:id", userController.updateUser, sendResponse("user"));
 router.get("/notifications/:userId", userController.todayNotifications);
