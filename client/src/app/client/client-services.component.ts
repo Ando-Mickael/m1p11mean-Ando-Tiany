8d067180d27963218211ed7e7d070479797bfcd4
SAMPILAHY HERINIAVO Tiany <sampilahy.heriniavo@gmail.com>
2024-02-29 21:40:21 +0300

    recherche

--- client/src/app/client/client-services.component.ts
+++ client/src/app/client/client-services.component.ts
@@ -7,11 +7,14 @@ import { ConfigService } from '../config.service';
     <section class="ftco-section bg-light">
       <div class="container">
         <div class="row">
+          <div class="col-12 mb-4">
+            <input type="text" [(ngModel)]="searchTerm" (ngModelChange)="filterServices()" placeholder="Rechercher une service" class="form-control">
+          </div>
           <div class="col-12 order-md-last" *ngIf="!servicesLoading">
             <div class="row">
               <div
                 class="col-sm-12 col-md-12 col-lg-4 ftco-animate d-flex"
-                *ngFor="let service of services"
+                *ngFor="let service of filteredServices"
               >
                 <app-service [service]="service"></app-service>
               </div>
@@ -24,7 +27,9 @@ import { ConfigService } from '../config.service';
 })
 export class ClientServicesComponent {
   services: any[] = [];
+  filteredServices: any[] = [];
   servicesLoading = true;
+  searchTerm: string = '';
 
   apiUrl: string;
 
@@ -41,7 +46,21 @@ export class ClientServicesComponent {
       .then((response) => response.json())
       .then((data) => {
         this.services = data;
+        this.filteredServices = data; // Initially set filteredServices to all services
         this.servicesLoading = false;
       });
+  }
+
+  // Function to filter services based on search term
+  filterServices() {
+    if (!this.searchTerm) {
+      // If search term is empty, show all services
+      this.filteredServices = this.services;
+    } else {
+      // Filter services by name based on search term
+      this.filteredServices = this.services.filter(service =>
+        service.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    }
   }
 }
